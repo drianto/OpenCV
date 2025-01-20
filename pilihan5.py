74e871a97f85bd3e437453030b77e125c6e7798b
drianto <andri070805@gmail.com>
2025-01-20 20:57:22 +0700

    feat: add ORB Feature Extraction

---
+++ pilihan5.py
@@ -0,0 +1,12 @@
+import cv2
+
+image = cv2.imread('toyotaInnovaZenix.jpg')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+keypoints, descriptors = cv2.ORB_create().detectAndCompute(gray_image, None)
+
+output_image = cv2.drawKeypoints(image, keypoints, None, color=(0, 255, 0))
+
+cv2.imshow('Picture ORB', output_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
